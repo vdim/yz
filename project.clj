b5794c367379fd6cb169eb49e9eee664b6c6be31
vdim <vyacheslav.dimitrov@gmail.com>
2011-07-29 10:52:57 +0400

    Added infrastructure for using lein build system

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject yz "0.0.1"
+  :description "Implementation of YZ language."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [fnparse "2.2.7"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]])
+
