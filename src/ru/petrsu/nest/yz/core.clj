db9fd3d1ea2e5cdbac6919d0b2ae5cc683858bfe
vdim <vyacheslav.dimitrov@gmail.com>
2011-07-29 10:50:52 +0400

    Added description of core file

---
+++ src/ru/petrsu/nest/yz/core.clj
@@ -0,0 +1,7 @@
+(ns ru.petrsu.nest.yz.core
+  ^{:author Vyacheslav Dimitrov
+    :doc "This code contains core functions of implementation of YZ language.
+         Parsing of queries does due to fnparse library."}
+  (:use name.choi.joshua.fnparse))
+
+
