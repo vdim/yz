2a887414b5abd450cbc3f2263597b43e02eff266
vdim <vyacheslav.dimitrov@gmail.com>
2011-08-04 20:39:27 +0400

    Fixed tests

--- test/ru/petrsu/nest/yz/test_core.clj
+++ test/ru/petrsu/nest/yz/test_core.clj
@@ -4,7 +4,8 @@
   (:use ru.petrsu.nest.yz.core clojure.contrib.test-is))
 
 (deftest t-get-class
-         ^{:doc "Tested get-class function"}
+         ^{:doc "Tests get-class function"}
          (is (= (get-class "java.lang.String") java.lang.String))
-         (is (= (get-class "java.lang.MyString") nil)))
+         (is (= (get-class "java.lang.MyString") nil))
+         (is (= (get-class "ru.petrsu.nest.son.Room") ru.petrsu.nest.son.Room)))
 
--- test/ru/petrsu/nest/yz/test_map_utils.clj
+++ test/ru/petrsu/nest/yz/test_map_utils.clj
@@ -5,9 +5,20 @@
 
 (def mom {:a {:sn "sn1", :dp "dp1"}, :b {:sn "sn2" :dp, "dp2"}})
 
-(deftest t-get-class
+(deftest t-find-in-sn
          ^{:doc "Tests find-in-sn function"}
          (is (= (find-in-sn "sn1" mom) :a))
          (is (= (find-in-sn "sn2" mom) :b))
          (is (= (find-in-sn "sn3" mom) nil)))
+
+
+(def nest-mom {ru.petrsu.nest.son.Room {:sn "r", :dp "number"}, 
+               ru.petrsu.nest.son.Floor {:sn "f" :dp, "number"}})
+
+(deftest t-find-in-sn-nest
+         ^{:doc "Tests find-in-sn function for nest MOM."}
+         (is (= (find-in-sn "r" nest-mom) ru.petrsu.nest.son.Room))
+         (is (= (find-in-sn "f" nest-mom) ru.petrsu.nest.son.Floor))
+         (is (= (find-in-sn "s" nest-mom) nil)))
+
 
