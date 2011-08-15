1a33258e975d085ee51f05390d82c72dca91dfb8
vdim <vyacheslav.dimitrov@gmail.com>
2011-08-15 12:21:31 +0400

    Implemented "find-class" function

--- src/ru/petrsu/nest/yz/parsing.clj
+++ src/ru/petrsu/nest/yz/parsing.clj
@@ -1,37 +1,80 @@
 (ns ru.petrsu.nest.yz.parsing
   ^{:author Vyacheslav Dimitrov
     :doc "Code for the parsing of queries does due to the fnparse library."}
-  (:use name.choi.joshua.fnparse))
+  (:use name.choi.joshua.fnparse)
+  (:require [clojure.string :as cs]))
+
+(declare find-class)
 
 ; The parsing state data structure. The rest of input string is stored
-; in :remainder, and vector of maps is stored in :result.
-(defstruct q-representation :remainder :result)
+; in :remainder, vector of maps is stored in :result, and the map of
+; the object model is stared in :mom.
+(defstruct q-representation :remainder :result :mom)
 
 ; Rules of grammar are below. See BNF in the begining of file.
 (def alpha
   ^{:doc "Sequence of characters."}
   (lit-alt-seq "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"))
 
+
 (def whitespaces
   ^{:doc "List of whitespaces"}
-  (rep+ (alt (lit \space) (lit \newline))))
+  (rep+ (alt (lit \space) (lit \newline) (lit \tab))))
+
+
+(defmacro sur-by-ws
+  "Surrounds 'rule' by whitespaces like this
+  (conc (opt whitespaces) rule (opt whitespaces))."
+  [rule]
+  `(conc (opt whitespaces) ~rule (opt whitespaces)))
+
 
 (def id 
   (complex [id# (rep+ alpha)
             g-res (get-info :result)
-            s-res (set-info :result (concat g-res [(reduce str id#)]))]
+            g-mom (get-info :mom)
+            _ (set-info :result [(assoc (g-res 0) :what (find-class (reduce str id#), g-mom))])]
            id#))
 
+
 (def delimiter
-  (alt (lit \.) (lit \,)))
+  ^{:doc "Define delimiter of ids: there are comma (for queries) 
+  and point (for property or link and so on)."}
+  (alt (lit \.) (sur-by-ws (lit \,))))
+
 
 (def query
-  (conc id (rep+ (conc delimiter id))))
+  (conc id (rep* (conc delimiter id))))
+
+
+(def empty-res
+  ^{:doc "Defines vector within one empty map. 
+         The vector is initial result of 'parse' function."}
+  [{:what nil
+   :props nil
+   :pred nil
+   :then nil
+   :next nil}])
+
 
 (defn parse
-  "Parses specified query on YZ language."
-  [q]
-  (query (struct q-representation (seq q) {})))
+  "Parses specified query ('q') on YZ language based on
+  specified ('mom') the map of the object model."
+  [q, mom]
+  (:result ((query (struct q-representation (seq q) empty-res mom)) 1)))
 
 
-(parse "building.room,device")
+(defn find-class
+  [id mom]
+  "Returns class which is correspended 'id' (search is did in 'mom'). 
+  Search is did in the following positions:
+    - as full name of class (package+name)
+    - as name of class 
+    - in 'sn' key of each map of mom.
+    - as abbreviation class's name."
+  (some (fn [el] (let [[cl m] el, b (bean cl), l-id (cs/lower-case id)]
+                   (if (or (= l-id (cs/lower-case (:name b))) 
+                           (.startsWith (cs/lower-case (:simpleName b)) l-id)
+                           (= l-id (cs/lower-case (:sn m))))
+                     cl))) 
+        mom))
--- test/ru/petrsu/nest/yz/test_parsing.clj
+++ test/ru/petrsu/nest/yz/test_parsing.clj
@@ -1,50 +1,26 @@
 (ns ru.petrsu.nest.yz.test-parsing
   ^{:author Vyacheslav Dimitrov
     :doc "Tests for parsing functions."}
-  (:use ru.petrsu.nest.yz.parsing clojure.contrib.test-is))
+  (:use ru.petrsu.nest.yz.parsing 
+        ru.petrsu.nest.yz.hb-utils 
+        clojure.contrib.test-is)
+  (:import (ru.petrsu.nest.son Building)))
 
-(def classes #{Floor, Room, Building})
+(def mom (gen-mom-from-cfg "/home/adim/tsen/clj/libs/yz/test/etc/hibernate.cfg.xml"))
+
+(deftest t-find-class
+         ^{:doc "Tests 'find-class' function."}
+         (is (= (find-class "ru.petrsu.nest.son.Building", mom) Building))
+         (is (= (find-class "ru.petrsu.nest.son.building", mom) Building))
+         (is (= (find-class "building", mom) Building))
+         (is (= (find-class "b", mom) Building))
+         (is (= (find-class "build", mom) Building)))
 
 (deftest t-parse
          ^{:doc "Tests 'parse' function."}
-          (is (= (parse "building")
+          (is (= (parse "building", mom)
                  [{:what ru.petrsu.nest.son.Building 
                    :props nil
                    :pred nil
                    :then nil
-                   :next nil}]))
-
-           (is (= (parse "building (room)")
-                 [{:what ru.petrsu.nest.son.Building 
-                   :props nil
-                   :pred nil
-                   :then nil
-                   :next [{:what ru.petrsu.nest.son.Room
-                           :pred nil
-                           :then nil
-                           :next nil}]}]))
-
-           (is (= (parse "building, room")
-                 [{:what ru.petrsu.nest.son.Building 
-                   :props nil
-                   :pred nil
-                   :then nil
-                   :next nil}]))
-         
-            (is (= (parse "floor.building")
-                 [{:what ru.petrsu.nest.son.Building 
-                   :props ["building"]
-                   :pred nil
-                   :then nil
-                   :next nil}]))
-
-            (is (= (parse "building.room")
-                 [{:what ru.petrsu.nest.son.Building 
-                   :props nil
-                   :pred nil
-                   :then {:what ru.petrsu.nest.son.Room
-                          :props nil
-                          :pred nil
-                          :then nil
-                          :next nil}
                    :next nil}])))
