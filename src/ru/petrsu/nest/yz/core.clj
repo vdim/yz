558d508c46398e1d149be3104b4cd7a3065f5d68
vdim <vyacheslav.dimitrov@gmail.com>
2011-08-18 00:25:00 +0400

    Minor changes

--- src/ru/petrsu/nest/yz/core.clj
+++ src/ru/petrsu/nest/yz/core.clj
@@ -2,17 +2,13 @@
   ^{:author Vyacheslav Dimitrov
     :doc "This code contains core functions of the Clojure's implementation of the YZ language.
          The Parsing of queries does due to the fnparse library."}
-  (:require [ru.petrsu.nest.yz.hb-utils :as hb]))
+  (:require [ru.petrsu.nest.yz.hb-utils :as hb]
+            [ru.petrsu.nest.yz.parsing :as p]))
 
 (def 
   ^{:doc "The map of the object model."}
   mom (hb/gen-mom-from-cfg "/home/adim/tsen/clj/libs/yz/test/etc/hibernate.cfg.xml"))
 
-(defn get-class
-  "Gets name of class, finds this class and returns instance of java.lang.Class.
-  If search of class fails then nil is returned."
-  [id]
-  (try 
-    (Class/forName id) 
-    (catch ClassNotFoundException _ nil)))
-
+(defn p
+  []
+  (p/parse "building.room" mom))
--- test/ru/petrsu/nest/yz/test_core.clj
+++ test/ru/petrsu/nest/yz/test_core.clj
@@ -3,9 +3,3 @@
     :doc "Tests for the core of the implementation of YZ."}
   (:use ru.petrsu.nest.yz.core clojure.contrib.test-is))
 
-(deftest t-get-class
-         ^{:doc "Tests get-class function"}
-         (is (= (get-class "java.lang.String") java.lang.String))
-         (is (= (get-class "java.lang.MyString") nil))
-         (is (= (get-class "ru.petrsu.nest.son.Room") ru.petrsu.nest.son.Room)))
-
