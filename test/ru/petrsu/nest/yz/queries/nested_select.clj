3caa946eab8c5ef830019ddfda646ed7efa9a20f
vdim <vyacheslav.dimitrov@gmail.com>
2011-08-21 00:06:02 +0400

    Added some tests

--- test/ru/petrsu/nest/yz/queries/nested_select.clj
+++ test/ru/petrsu/nest/yz/queries/nested_select.clj
@@ -9,8 +9,21 @@
 
 ;; Define model
 
-(def b1 (doto (Building.) (.addFloor (Floor.)) (.addFloor (Floor.))))
-(def b2 (doto (Building.) (.addFloor (Floor.))))
+(def f1_b1 (doto (Floor. 1) 
+             (.addRoom (Room. "101")) 
+             (.addRoom (Room. "102"))))
+
+(def f2_b1 (doto (Floor. 2) 
+             (.addRoom (Room. "201")) 
+             (.addRoom (Room. "202"))))
+
+(def f1_b2 (doto (Floor. 1) 
+             (.addRoom (Room. "1001")) 
+             (.addRoom (Room. "1002"))))
+
+
+(def b1 (doto (Building.) (.addFloor f1_b1) (.addFloor f2_b1)))
+(def b2 (doto (Building.) (.addFloor f1_b2)))
 
 (def son (doto (SON.)
            (.addBuilding b1) 
@@ -32,7 +45,30 @@
          (let [q (run-query "building (floor)" tc/mom *em*)]
            (is (= 1 (count q)))
            (is (= 2 (count (q 0))))
-           (is (not (nil? (get (q 0) b1))))
-           (is (= 1 (count (get (q 0) b1))))
-           (is (= 2 (count ((get (q 0) b1) 0)))))) ; Check number of floor for object b1.
+           (let [f-b1 (get (q 0) b1), ; f-b1 is floors of b1 building
+                 f-b2 (get (q 0) b2)] ; f-b2 is floors of b2 building
+             (is (not (nil? f-b1)))
+             (is (= 1 (count f-b1)))
+             (is (= 2 (count (f-b1 0)))) ; Check the number of floors for object b1.
+             (is (every? #(instance? Floor %) (keys (f-b1 0))))  ; Check class of floors for object b1.
+             (is (not (nil? f-b2)))
+             (is (= 1 (count f-b2)))
+             (is (= 1 (count (f-b2 0)))) ; Check the number of floors for object b2.
+             (is (every? #(instance? Floor %) (keys (f-b2 0)))))))  ; Check class of floors for object b1.
+
+(deftest select-floors-and-rooms
+         ^{:doc "Selects all Building and its Room objects."}
+         (let [q (run-query "building (room)" tc/mom *em*)]
+           (is (= 1 (count q)))
+           (is (= 2 (count (q 0))))
+           (let [r-b1 (get (q 0) b1), ; r-b1 is rooms of b1 building
+                 r-b2 (get (q 0) b2)] ; r-b2 is rooms of b2 building
+             (is (not (nil? r-b1)))
+             (is (= 1 (count r-b1)))
+             (is (= 4 (count (r-b1 0)))) ; Check the number of rooms for object b1.
+             (is (every? #(instance? Room %) (keys (r-b1 0))))  ; Check class of rooms for object b1.
+             (is (not (nil? r-b2)))
+             (is (= 1 (count r-b2)))
+             (is (= 2 (count (r-b2 0)))) ; Check the number of rooms for object b2.
+             (is (every? #(instance? Room %) (keys (r-b2 0)))))))  ; Check class of rooms for object b1.
 
