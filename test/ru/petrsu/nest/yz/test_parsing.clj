6097606275292b809919db9f8ca3b6ee24296191
vdim <vyacheslav.dimitrov@gmail.com>
2011-09-21 17:57:14 +0400

    Added tests for YZ`s functions

--- test/ru/petrsu/nest/yz/test_parsing.clj
+++ test/ru/petrsu/nest/yz/test_parsing.clj
@@ -48,96 +48,6 @@
 (def fun
   "ru.petrsu.nest.yz.core/process-preds ")
 
-(def qlist
-  ^{:doc "Defines list of YZ's queries (used Nest's model)."}
-  [
-;; Selections   
-   "building"
-   "building.room"
-   "building.floor.room"
-   "building.floor.room.occupancy"
-   "building, room"
-   "building (room)"
-   "building (room (device))"
-   "building (room (device (networkinterface)))"
-   "building (room.floor (device))"
-   "building (room.floor (device), occupancy)"
-   "building.floor.room (room.floor.building, network (device.building), occupancy)"
-   "building.floor.room (room.floor.building (device.building), occupancy)"
-   "building.name"
-   "building.floors"
-   "building.room.floor.rooms"
-   "building (room.number)"
-   "building (device.forwarding)"
-   "building (room.device.forwarding)"
-   "building (room.device.forwarding, floor)"
-   "building (room.device.forwarding, floor, network.building.floors)"
-   "building (room.device.forwarding, floor (network.building.floors))"
-
-;; Restrictions.
-   "floor#(number=1)"
-   "floor#(number=1 or number=2)"
-   "floor#(number=(1 or 2))"
-   "floor#(number=(1 or (2 or 3)))"
-   "floor#(number=(1 and (2 or 3)))"
-   "floor#(number=(1 and (2 or 3 and 4)))"
-   "floor#(number=1) (room)"
-   "floor#(number=1) (room (device))"
-   "floor#(number=1) (room (device)), building"
-   "floor#(number=1).room"
-   "floor#(number=(1 and (2 or 3 and 4))).room.building.name"
-   "floor#(number=1 and name=2)"
-   "floor#(number=1 or name=2)"
-   "floor#((number=1 or number=2) and name=3)"
-   "floor#((number=1 or number=2) and name=3), room"
-   "floor#((number=1 or number=2) and name=3), room#(number=1)"
-   "floor#((number=1 or number=2) and name=3), room#(number=1 and name=2)"
-   "floor#((number=1 or number=2) and name=3) (room#(number=1))"
-   "floor#((number=1 or number=2) and name=3) (room#(number=1 and name=2))"
-
-;; Properties.
-   "floor[name]"
-   "floor[name number]"
-   "floor[name number rooms]"
-   "room[name number]"
-   "room[name number floor]"
-   "room[name number floor].floor"
-   "room[name number floor].floor[number]"
-   "room[name number floor].floor[number name rooms]"
-   "room.floor[number name rooms]"
-   "room[name number floor].floor[number name rooms].building"
-   "room[name number floor].floor[number name rooms].building[name]"
-   "room[name number floor].floor[number name rooms].building[name address]"
-   "room[name number floor].floor[number name rooms].building[name address floors]"
-   "room.floor[number name rooms].building[name address floors]"
-   "room.floor.building[name address floors]"
-   "simpleou[*parent]"
-   "simpleou[*parent name]"
-   "compositeou[*parent OUs]"
-   "building (simpleou[*parent])"
-   "building[name] (simpleou[*parent])"
-   "building[name] (simpleou[*parent name])"
-   "building[&]"
-   "building[& name]"
-   "building[name &]"
-   "building[name floors &]"
-   "building[name & floors]"
-   "building[& name floors]"
-   "building (room[&])"
-   "building (room[number &])"
-   "building (room[& number])"
-   "building[name] (room[& number])"
-   "building[name] (room[number &])"
-   "building[name &] (room[number &])"
-   "room"])
-
-(deftest parse-remainder
-         ^{:doc "Checks remainder after parsing for queries in 'qlist' vector.
-                It must be nil for all queries, because qlist contains
-                only correct queries."}
-         (is (nil? (some #(not (nil? %)) (map #(:remainder (parse+ % mom)) qlist)))))
-
-
 (deftest t-parse
          ^{:doc "Tests 'parse' function."}
           (is (= (parse "building", mom)
@@ -446,8 +356,179 @@
 
 (defmacro create-is [q mom] `(is (nil? (:remainder (parse+ ~q ~mom)))))
 
+(def qlist
+  ^{:doc "Defines list of YZ's queries (used Nest's model)."}
+  [
+;; Selections   
+   "building"
+   "building.room"
+   "building.floor.room"
+   "building.floor.room.occupancy"
+   "building, room"
+   "building (room)"
+   "building (room (device))"
+   "building (room (device (networkinterface)))"
+   "building (room.floor (device))"
+   "building (room.floor (device), occupancy)"
+   "building.floor.room (room.floor.building, network (device.building), occupancy)"
+   "building.floor.room (room.floor.building (device.building), occupancy)"
+   "building.name"
+   "building.floors"
+   "building.room.floor.rooms"
+   "building (room.number)"
+   "building (device.forwarding)"
+   "building (room.device.forwarding)"
+   "building (room.device.forwarding, floor)"
+   "building (room.device.forwarding, floor, network.building.floors)"
+   "building (room.device.forwarding, floor (network.building.floors))"
+
+;; Restrictions.
+   "floor#(number=1)"
+   "floor#(number=1 or number=2)"
+   "floor#(number=(1 or 2))"
+   "floor#(number=(1 or (2 or 3)))"
+   "floor#(number=(1 and (2 or 3)))"
+   "floor#(number=(1 and (2 or 3 and 4)))"
+   "floor#(number=1) (room)"
+   "floor#(number=1) (room (device))"
+   "floor#(number=1) (room (device)), building"
+   "floor#(number=1).room"
+   "floor#(number=(1 and (2 or 3 and 4))).room.building.name"
+   "floor#(number=1 and name=2)"
+   "floor#(number=1 or name=2)"
+   "floor#((number=1 or number=2) and name=3)"
+   "floor#((number=1 or number=2) and name=3), room"
+   "floor#((number=1 or number=2) and name=3), room#(number=1)"
+   "floor#((number=1 or number=2) and name=3), room#(number=1 and name=2)"
+   "floor#((number=1 or number=2) and name=3) (room#(number=1))"
+   "floor#((number=1 or number=2) and name=3) (room#(number=1 and name=2))"
+
+;; Properties.
+   "floor[name]"
+   "floor[name number]"
+   "floor[name number rooms]"
+   "room[name number]"
+   "room[name number floor]"
+   "room[name number floor].floor"
+   "room[name number floor].floor[number]"
+   "room[name number floor].floor[number name rooms]"
+   "room.floor[number name rooms]"
+   "room[name number floor].floor[number name rooms].building"
+   "room[name number floor].floor[number name rooms].building[name]"
+   "room[name number floor].floor[number name rooms].building[name address]"
+   "room[name number floor].floor[number name rooms].building[name address floors]"
+   "room.floor[number name rooms].building[name address floors]"
+   "room.floor.building[name address floors]"
+   "simpleou[*parent]"
+   "simpleou[*parent name]"
+   "compositeou[*parent OUs]"
+   "building (simpleou[*parent])"
+   "building[name] (simpleou[*parent])"
+   "building[name] (simpleou[*parent name])"
+   "building[&]"
+   "building[& name]"
+   "building[name &]"
+   "building[name floors &]"
+   "building[name & floors]"
+   "building[& name floors]"
+   "building (room[&])"
+   "building (room[number &])"
+   "building (room[& number])"
+   "building[name] (room[& number])"
+   "building[name] (room[number &])"
+   "building[name &] (room[number &])"
+
+;; Functions as query.
+   "@(str \"One - \" 1 \", Two -\" 2)"
+   "@(str `room')"
+   "@(str %room')"
+   "@(str $room')"
+   "@(str `floor')"
+   "@(str %floor')"
+   "@(str $floor')"
+   "@(str `floor' `room')"
+   "@(str `floor' %room')"
+   "@(str `floor' $room')"
+   "@(str %floor' %room')"
+   "@(str %floor' `room')"
+   "@(str %floor' $room')"
+   "@(str $floor' $room')"
+   "@(str $floor' `room')"
+   "@(str $floor' %room')"
+   "@(str `floor' 1 `room')"
+   "@(str `floor' 2 $room')"
+   "@(str `floor' 3 %room')"
+   "@(str `floor' 1 `room' 3)"
+   "@(str `floor' 2 $room' 2)"
+   "@(str `floor' 3 %room' 1)"
+   "@(str `floor' `room' 3)"
+   "@(str `floor' $room' 2)"
+   "@(str `floor' %room' 1)"
+   "@(str 1 `floor' `room' 3)"
+   "@(str 2 `floor' $room' 2)"
+   "@(str 3 `floor' %room' 1)"
+   "@(str 1 `floor' 4 `room' 3)"
+   "@(str 2 `floor' 5 $room' 2)"
+   "@(str 3 `floor' 6 %room' 1)"
+   "room"])
+
+(def qlist-list
+  ^{:doc "Defines list with query-function with parameter (as :list) from qlist"}
+  (vec (map #(str "@(str `" % "')") qlist)))
+
+(def qlist-single
+  ^{:doc "Defines list with query-function with parameter (as :single) from qlist"}
+  (vec (map #(str "@(str $" % "')") qlist)))
+
+(def qlist-indep
+  ^{:doc "Defines list with query-function with parameter (as :indep) from qlist"}
+  (vec (map #(str "@(str %" % "')") qlist)))
+
+(def qlist-prop
+  ^{:doc "Defines list with queries which have function as property."}
+  (vec (flatten (list 
+                  (map #(str "building[& @(str `" % "')]") qlist)
+                  (map #(str "building[@(str `" % "') &]") qlist)
+                  (map #(str "building[@(str `" % "') & @(str `" % "')]") qlist)
+                  (map #(str "building[& @(str `" % "') @(str `" % "')]") qlist)
+                  (map #(str "building[@(str `" % "') @(str `" % "') &]") qlist)
+                  (map #(str "building[& @(str $" % "')]") qlist)
+                  (map #(str "building[& @(str %" % "')]") qlist)))))
+
+(def qlist-pred
+  ^{:doc "Defines list with queries which have function as predicate."}
+  (vec (flatten (list 
+                  (map #(str "building#(@(str `" % "') > 3)") qlist)
+                  (map #(str "building#(@(str `" % "') < 3)") qlist)
+                  (map #(str "building#(@(str `" % "') = @(str $" % "'))") qlist)
+                  (map #(str "building#(@(str $" % "') = @(str %" % "'))") qlist)))))
+
+(defmacro premainder
+  "Generates code for checking remainder about specified list with queries."
+  [l]
+  `(is (nil? (some #(not (nil? %)) (map #(:remainder (parse+ % mom)) ~l)))))
+
+(deftest parse-remainder
+         ^{:doc "Checks remainder after parsing for queries in 'qlist' vector.
+                It must be nil for all queries, because qlist contains
+                only correct queries."}
+         (premainder qlist)
+         (premainder qlist-list)
+         (premainder qlist-indep)
+         (premainder qlist-prop)
+         (premainder qlist-pred)
+         (premainder qlist-single))
+
+
 (deftest t-parse-remainder
-         ^{:doc "Denerates is for each query from qlist. 
+         ^{:doc "Generates 'is' for each query from qlist. 
                 Like 'parse-remainder, but it can show remainder.'"}
-         (dotimes [n (count qlist)]
-           (create-is (qlist n) mom)))
+         (let [f #(dotimes [n (count %)]
+                   (create-is (% n) mom))]
+           (f qlist)
+           (f qlist-indep)
+           (f qlist-single)
+           (f qlist-prop)
+           (f qlist-pred)
+           (f qlist-list)))
+
