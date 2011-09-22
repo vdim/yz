95b4d7f10ae531a863680ab03b0bd270d97ed357
vdim <vyacheslav.dimitrov@gmail.com>
2011-09-22 09:03:03 +0400

    Added minl function

--- src/ru/petrsu/nest/yz/functions.clj
+++ src/ru/petrsu/nest/yz/functions.clj
@@ -1,11 +1,17 @@
 (ns ru.petrsu.nest.yz.functions
   ^{:author Vyacheslav Dimitrov
     :doc "Set of functions which is build-in YZ."}
-  (:import (java.lang String)))
+  (:use clojure.core))
 
 
 (defn maxl
   "Takes a list with tuples which have one element and its
-  value is numeric."
+  value is numeric ant returns max element from this list."
   [tuples]
   (reduce max (flatten tuples)))
+
+(defn minl
+  "Takes a list with tuples which have one element and its
+  value is numeric and returns min element from this list."
+  [tuples]
+  (reduce min (flatten tuples)))
