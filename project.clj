a211975a1b1522f9b086680bafcc7238c407141c
vdim <vyacheslav.dimitrov@gmail.com>
2011-10-02 23:27:23 +0400

    Added infrastructure for aot compilation.

--- project.clj
+++ project.clj
@@ -3,5 +3,9 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [fnparse "2.2.7"]]
+  :aot [ru.petrsu.nest.yz.core 
+        ru.petrsu.nest.yz.hb-utils 
+        ru.petrsu.nest.yz.functions]
+  :jar-exclusions [#"\.swp$|\.clj$"]
   :dev-dependencies [[lein-clojars "0.5.0"]])
 
