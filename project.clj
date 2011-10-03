ed7467238af3719d9f7dbb4a9064c1f55488df1b
vdim <vyacheslav.dimitrov@gmail.com>
2011-10-03 13:03:55 +0400

    Added hibernate dependencies for testing YZ.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject yz "0.0.1"
   :description "Implementation of YZ language."
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [fnparse "2.2.7"]]
+                 [fnparse "2.2.7"]
+                 [org.hibernate/hibernate-entitymanager "3.6.1.Final"]]
   :aot [ru.petrsu.nest.yz.core 
         ru.petrsu.nest.yz.hb-utils 
         ru.petrsu.nest.yz.functions]
