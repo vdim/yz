7f2fab35e40c98b8f0bef873fb78f9e2d1e05453
vdim <vyacheslav.dimitrov@gmail.com>
2011-10-04 19:07:26 +0400

    Added project`s url.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject yz "0.0.1-alpha"
+(defproject yz "0.0.1-alpha1"
   :description "Implementation of YZ language."
+  :url "https://github.com/vdim/yz"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.hibernate/hibernate-entitymanager "3.6.1.Final"]
                  [org.apache.derby/derby "10.7.1.1"]
