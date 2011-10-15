7be034c7085bf278756ea0726e35df6ce9202a37
vdim <vyacheslav.dimitrov@gmail.com>
2011-10-15 11:26:57 +0400

    Added copyright.

--- src/ru/petrsu/nest/yz/hb_utils.clj
+++ src/ru/petrsu/nest/yz/hb_utils.clj
@@ -1,3 +1,23 @@
+;;
+;; Copyright 2011 Vyacheslav Dimitrov <vyacheslav.dimitrov@gmail.com>
+;;
+;; This file is part of YZ.
+;;
+;; YZ is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU Lesser General Public License version 3
+;; only, as published by the Free Software Foundation.
+;;
+;; YZ is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with YZ.  If not, see
+;; <http://www.gnu.org/licenses/>.
+;;
+
+
 ; This functions are for working with hibernate as
 ; user (at least we use hibernate in nest) 
 ; can use hibernate as framework between its 
--- test/ru/petrsu/nest/yz/queries/preds.clj
+++ test/ru/petrsu/nest/yz/queries/preds.clj
@@ -1,3 +1,22 @@
+;;
+;; Copyright 2011 Vyacheslav Dimitrov <vyacheslav.dimitrov@gmail.com>
+;;
+;; This file is part of YZ.
+;;
+;; YZ is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU Lesser General Public License version 3
+;; only, as published by the Free Software Foundation.
+;;
+;; YZ is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with YZ.  If not, see
+;; <http://www.gnu.org/licenses/>.
+;;
+
 (ns ru.petrsu.nest.yz.queries.preds
   ^{:author "Vyacheslav Dimitrov"
     :doc "Processes queries within some restrictions."}
