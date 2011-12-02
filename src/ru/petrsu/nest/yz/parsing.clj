ae7949e1a7f733365c903907472bad2930b3c81c
vdim <vyacheslav.dimitrov@gmail.com>
2011-12-02 12:54:36 +0300

    Fixed grammar.

--- src/ru/petrsu/nest/yz/parsing.clj
+++ src/ru/petrsu/nest/yz/parsing.clj
@@ -936,11 +936,12 @@
   ^{:doc "Defines start symbol for parsing query."}
   (alt (conc funcq ; Query may be simple function: @(count `room')
              (rep* (conc delimiter query))); ; or function + some query: @(count `room'), room
-       (rep+ ; or  sequence from the following parts.
-         (alt 
-           bid ; Id's sequence with properties or predicates: room; room.floor; room[name]; room#(name="MB") and so on.
-           nest-query ; Nested query: room (floor)
-           (conc delimiter query))))) 
+
+       (conc bid ; Id's sequence with properties or predicates: room; room.floor; room[name]; room#(name="MB") and so on.
+         (rep* ; or  sequence from the following parts.
+           (alt
+             nest-query ; Nested query: room (floor)
+             (conc delimiter query)))))) ; Next query: room, floor
 
 
 (defn parse+
