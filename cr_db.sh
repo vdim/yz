40c83f3ea5b2a053cf67a324d3bb661be87fb2fa
vdim <vyacheslav.dimitrov@gmail.com>
2012-02-19 22:50:47 +0300

    Improved cr_db script.

--- cr_db.sh
+++ cr_db.sh
@@ -3,32 +3,55 @@
 # Copyright (C) 2012 Vyacheslav Dimitrov, Petrozavodsk State University
 #
 # Generates databases.
-# Usage: ./cr_db [db_name]
-# db_hame may be h2, derby, hsqldb, lsm (lsm by default).
 #
 
 # Set with amount elements of database.
-n_bd="1000 5000 10000 15000 20000 50000 100000"
-
-## Classpath
-CP=`lein classpath`
+n_db="1000 5000 10000 15000 20000 50000 100000"
 
 clj_file="test/ru/petrsu/nest/yz/benchmark/benchmark.clj"
 clj_func="ru.petrsu.nest.yz.benchmark.benchmark/generate-bd"
 
-## Derby settings.
+# Derby settings.
 dialect_derby="org.hibernate.dialect.DerbyDialect"
 driver_derby="org.apache.derby.jdbc.EmbeddedDriver"
 
-## H2 settings.
+# H2 settings.
 dialect_h2="org.hibernate.dialect.H2Dialect"
 driver_h2="org.h2.Driver"
 
-## HSQLDB settings.
+# HSQLDB settings.
 dialect_hsqldb="org.hibernate.dialect.HSQLDialect"
 driver_hsqldb="org.hsqldb.jdbcDriver"
 
-for n in $n_bd; do
+# Define default database.
+database="h2"
+
+# Help string
+usage="Usage: $0 [OPTION...]
+Generate database of specified type for specified amount elements.
+
+Options:
+    -d, --database <database>   database (h2, derby, hsqldb, lsm). h2 by default.
+    -n, --elems-database <\"el1 el2 ...\"> list with amount elements into databases
+				(all by default).
+    -h, --help			display this help message and exit"
+
+# Handling options.
+while true; do
+    case "$1" in
+        -d|--database) database=$2; shift 2;;
+        -h|--help) echo "$usage"; exit 0 ;; 
+	-n|--elems-database) n_db=$2; shift 2;;
+        -*) echo "unknown option $1" >&2 ; exit 1 ;;
+	*) break ;;
+    esac
+done
+
+
+# Classpath
+CP=`lein classpath`
+
+for n in $n_db; do
 	echo $n
 
 	# Derby url.
@@ -48,15 +71,13 @@ for n in $n_bd; do
 	lsm="$url_lsm"
 
 	# Define current connection string.
-	url=$lsm
-	if test "$#" -gt 0; then
-	    case "$1" in
-		"h2") url=$h2 ;;
-		"derby") url=$derby ;;
-		"hsqldb") url=$hsqldb ;;
-		"lsm") url=$lsm ;;
-	    esac
-	fi;
+	url=$h2
+	case "$database" in
+	    "h2") url=$h2 ;;
+	    "derby") url=$derby ;;
+	    "hsqldb") url=$hsqldb ;;
+	    "lsm") url=$lsm ;;
+	esac
 
 	# Run generate-bd function from ru.petrsu.nest.yz.benchmark.benchmark namespace. 
 	# For more details see doc string for the clojure.main/main function.
