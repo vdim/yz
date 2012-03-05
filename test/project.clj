30e812456c3e61f96a635a46fc76d2b6e56a0f04
vdim <vyacheslav.dimitrov@gmail.com>
2012-03-05 09:03:19 +0300

    Removed yz dependency.

--- test/project.clj
+++ test/project.clj
@@ -8,8 +8,7 @@
                  [org.hsqldb/hsqldb "2.2.4"]
                  [org.clojure/algo.monads "0.1.0"] 
                  [incanter/incanter-charts "1.3.0-SNAPSHOT"]
-                 [incanter/incanter-core "1.3.0-SNAPSHOT"]
-                 [ru.petrsu.nest/yz "0.0.1-alpha11"]]
+                 [incanter/incanter-core "1.3.0-SNAPSHOT"]]
   :java-source-path "src/"
   :dev-dependencies [[lein-clojars "0.7.0"]]
   :warn-on-reflection false
