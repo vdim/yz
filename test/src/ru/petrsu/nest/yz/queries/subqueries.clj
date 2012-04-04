48c52932f4bd09b565b070a7fdac35a5d23b7d7c
vdim <vyacheslav.dimitrov@gmail.com>
2012-04-05 00:39:13 +0400

    Added tests for Ŷ and ∀ modificators.

--- test/src/ru/petrsu/nest/yz/queries/subqueries.clj
+++ test/src/ru/petrsu/nest/yz/queries/subqueries.clj
@@ -27,9 +27,10 @@
   (:import (ru.petrsu.nest.son 
              SON, Building, Room, Floor)))
 
-(def r_101 (doto (Room.) (.setNumber "101")))
+(def r_101 (doto (Room.) (.setDescription "f1_b1") (.setNumber "101")))
 (def r_102 (doto (Room.) (.setNumber "102")))
 (def f1_b1 (doto (Floor.) 
+             (.setName "f1_b1")
              (.setDescription "SM")
              (.setNumber (Integer. 1))
              (.addRoom r_101) (.addRoom r_102)))
@@ -40,11 +41,13 @@
              (.addRoom (doto (Room.) (.setNumber "201"))) 
              (.addRoom (doto (Room.) (.setNumber "202")))))
 
+(def r_1001 (doto (Room.) (.setDescription "f1_b2") (.setNumber "1001")))
+(def r_1002 (doto (Room.) (.setDescription "f1_b2") (.setNumber "1002")))
 (def f1_b2 (doto (Floor.) 
              (.setName "f1_b2")
              (.setNumber (Integer. 1))
-             (.addRoom (doto (Room.) (.setNumber "1001"))) 
-             (.addRoom (doto (Room.) (.setNumber "1002")))))
+             (.addRoom r_1001) 
+             (.addRoom r_1002)))
 
 (def b1 (doto (Building.) 
           (.setName "building") (.setDescription "101")
@@ -54,6 +57,7 @@
           (.addFloor f1_b2)))
 
 (def son (doto (SON.)
+           (.setName "SM")
            (.addBuilding b1) 
            (.addBuilding b2)))
 
@@ -71,7 +75,9 @@
            (is (= (f "floor#(description = Ŷbuilding.name)") [f1_b1]))
            (is (= (f "floor#(building = Ŷbuilding#(name=\"SM\"))") [f1_b2]))
            (is (= (f "floor#(description = Ŷbuilding)") []))
-           (is (tc/eq-colls (f "floor#(room = Ŷroom#(number~\"^2.*\"))") [f2_b1]))))
+           (is (tc/eq-colls (f "floor#(room = Ŷroom#(number~\"^2.*\"))") [f2_b1]))
+           (is (= (f "floor#(description=Ŷbuilding[name])") [f1_b1]))
+           (is (= (f "floor#(description=Ŷbuilding.name)") [f1_b1]))))
 
 
 (deftest cycling
@@ -96,10 +102,11 @@
            (is (tc/eq-colls (f "floor (room#(number = building.description))") [f1_b1 f2_b1 f1_b2 r_101]))
            (is (= (f "room#(number = building.description)") [r_101]))
            (is (= (f "building#(description = room.number)") [b1]))
-           (is (= (f "building#(description != ∀room.number)") [b2]))))
+           (is (= (f "building#(description != ∀room.number)") [b2]))
+           (is (= (f "building#(description!=∀room.number)") [b2]))))
 
 
-(deftest typed-any-modificator
+(deftest typed-all-modificator
          (let [f #(flatten (tc/rows-query %1))]
            (is (= (f "floor#(description = A:building[name])") [f1_b1]))
            (is (= (f "floor#(description = A:building.name)") [f1_b1]))
@@ -126,5 +133,110 @@
            (is (= (f "building#(description = (room.number and \"SomeDesc\"))") []))
            (is (tc/eq-colls (f "building#(description = (room.number or \"102\"))") [b1 b2]))
            (is (tc/eq-colls (f "building#(description != (\"102\" and ∀room.number))") []))
-           (is (tc/eq-colls (f "building#(description != (\"102\" and =room.number))") [b1]))
-           ))
+           (is (tc/eq-colls (f "building#(description != (\"102\" and =room.number))") [b1]))))
+
+
+(deftest not-any-modificator
+         (let [f #(flatten (tc/rows-query %1))]
+           (is (tc/eq-colls (f "floor#(name = room[description])") [f1_b1 f1_b2]))
+           (is (= (f "floor#(name = ∀room[description])") [f1_b2]))
+           (is (= (f "room#(number = ∀building.description)") [r_101]))
+
+           (is (= (f "building#(description = ∀room.number)") []))
+           (is (= (f "building#(description = ∀room.number || name=\"SomeName\")") []))
+           (is (= (f "building#(description = ∀room.number || name=\"SM\")") [b2]))
+           (is (= (f "building#(description = ∀room.number && name=\"SM\")") []))
+           (is (= (f "building#(description = ∀room.number || description=\"SomeDesc\")") []))
+           (is (= (f "building#(description = ∀room.number || description=\"102\")") [b2]))
+           (is (= (f "building#(description = ∀room.number && description=\"102\")") []))
+           (is (= (f "building#(description = (∀room.number || \"SomeDesc\"))") []))
+           (is (= (f "building#(description = (∀room.number || \"102\"))") [b2]))
+           (is (= (f "building#(description = (∀room.number && \"102\"))") []))
+           (is (= (f "building#(description = (\"SomeDesc\" || ∀room.number))") []))
+           (is (= (f "building#(description = (\"102\" || ∀room.number))") [b2]))
+           (is (= (f "building#(description = (\"102\" && ∀room.number))") []))
+
+           (is (= (f "floor#(name != ∀room[description])") [f2_b1]))
+           (is (tc/eq-colls (f "floor#(name != room[description])") [f1_b1 f2_b1]))
+           (is (= (f "floor#(name = (\"SomeName\" || != ∀room[description]))") [f2_b1]))
+           (is (= (f "floor#(name=(\"SomeName\" || !=∀room[description]))") [f2_b1]))
+           (is (= (f "floor#(name=(\"SomeName\" && !=∀room[description]))") []))
+           (is (tc/eq-colls (f "floor#(name=(\"f1_b2\" || !=∀room[description]))") [f2_b1 f1_b2]))
+           (is (= (f "floor#(name!=(\"SomeName\" && ∀room[description]))") [f2_b1]))
+           (is (tc/eq-colls (f "floor#(name!=(\"f1_b2\" || ∀room[description]))") [f2_b1 f1_b1]))
+           
+           (is (tc/eq-colls (f "floor#(name!=(\"f1_b2\" || \"f1_b1\" || ∀room[description]))") 
+                            [f2_b1 f1_b1 f1_b2]))
+           (is (tc/eq-colls (f "floor#(name!=(\"f1_b2\" && \"f1_b1\" || ∀room[description]))") 
+                            [f2_b1]))
+           (is (tc/eq-colls (f "floor#(name!=(\"f1_b2\" && (\"f1_b1\" || ∀room[description])))") 
+                            [f2_b1]))
+           (is (tc/eq-colls (f "floor#(name=(\"f1_b2\" && \"f1_b1\" || ∀room[description]))") 
+                            [f1_b2]))
+           (is (tc/eq-colls (f "floor#(name=(\"f1_b2\" && (\"f1_b1\" || ∀room[description])))") 
+                            [f1_b2]))
+           (is (tc/eq-colls (f "floor#(name!=(\"f1_b2\" or \"f1_b1\" or ∀room[description]))") 
+                            [f2_b1 f1_b1 f1_b2]))
+           (is (tc/eq-colls (f "floor#(name!=(\"f1_b2\" and \"f1_b1\" or ∀room[description]))") 
+                            [f2_b1]))
+           (is (tc/eq-colls (f "floor#(name!=(\"f1_b2\" and (\"f1_b1\" or ∀room[description])))") 
+                            [f2_b1]))
+           (is (tc/eq-colls (f "floor#(name=(\"f1_b2\" and \"f1_b1\" or ∀room[description]))") 
+                            [f1_b2]))
+           (is (tc/eq-colls (f "floor#(name=(\"f1_b2\" and (\"f1_b1\" or ∀room[description])))") 
+                            [f1_b2]))))
+
+
+(deftest not-any-and-all
+         (let [f #(flatten (tc/rows-query %1))]
+           (is (= (f "building#(name = Ŷ∀son[name])") [b2]))
+           (is (= (f "building#(name = ∀Ŷson[name])") [b2]))
+           (is (= (f "building#(name = Ŷson[name])") [b2]))
+           (is (= (f "building#(name = Ŷson[name])") [b2]))
+           (is (= (f "building#(name=Ŷ∀son[name])") [b2]))
+           (is (= (f "building#(name=∀Ŷson[name])") [b2]))
+           (is (= (f "building#(name=Ŷson[name])") [b2]))
+           (is (= (f "building#(name=Ŷson[name])") [b2]))
+           
+           (is (= (f "son#(name = Ŷ∀building[name])") []))
+           (is (= (f "son#(name = ∀Ŷbuilding[name])") []))
+           (is (= (f "son#(name = Ŷbuilding[name])") [son]))
+           (is (= (f "son#(name = Ŷbuilding[name])") [son]))
+           (is (= (f "son#(name=Ŷ∀building[name])") []))
+           (is (= (f "son#(name=∀Ŷbuilding[name])") []))
+           (is (= (f "son#(name=Ŷbuilding[name])") [son]))
+           (is (= (f "son#(name=Ŷbuilding[name])") [son]))))
+
+
+(deftest typed-not-any-and-all
+         (let [f #(flatten (tc/rows-query %1))]
+           (is (= (f "building#(name = A:all:son[name])") [b2]))
+           (is (= (f "building#(name = all:A:son[name])") [b2]))
+           (is (= (f "building#(name=A:all:son[name])") [b2]))
+           (is (= (f "building#(name=all:A:son[name])") [b2]))
+           
+           (is (= (f "son#(name = A:all:building[name])") []))
+           (is (= (f "son#(name = all:A:building[name])") []))
+           (is (= (f "son#(name=A:all:building[name])") []))
+           (is (= (f "son#(name=all:A:building[name])") []))))
+
+
+(deftest mixed-not-any-and-all
+         (let [f #(flatten (tc/rows-query %1))]
+           (is (= (f "building#(name = A:∀son[name])") [b2]))
+           (is (= (f "building#(name = ∀A:son[name])") [b2]))
+           (is (= (f "building#(name=A:∀son[name])") [b2]))
+           (is (= (f "building#(name=∀A:son[name])") [b2]))
+           (is (= (f "building#(name = Ŷall:son[name])") [b2]))
+           (is (= (f "building#(name = all:Ŷson[name])") [b2]))
+           (is (= (f "building#(name=Ŷall:son[name])") [b2]))
+           (is (= (f "building#(name=all:Ŷson[name])") [b2]))
+           
+           (is (= (f "son#(name = A:∀building[name])") []))
+           (is (= (f "son#(name = ∀A:building[name])") []))
+           (is (= (f "son#(name=A:∀building[name])") []))
+           (is (= (f "son#(name=∀A:building[name])") []))
+           (is (= (f "son#(name = Ŷall:building[name])") []))
+           (is (= (f "son#(name = all:Ŷbuilding[name])") []))
+           (is (= (f "son#(name=Ŷall:building[name])") []))
+           (is (= (f "son#(name=all:Ŷbuilding[name])") []))))
