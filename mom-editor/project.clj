fe949e32be18ee579665b8aeebd652ac54eaaa98
vdim <vyacheslav.dimitrov@gmail.com>
2012-04-19 11:31:18 +0400

    [MOMEd] Added code for starting REPL.

--- mom-editor/project.clj
+++ mom-editor/project.clj
@@ -1,3 +1,6 @@
 (defproject mom-editor "0.0.1-alpha"
   :description "MOM's editor."
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"] 
+                 [com.google.guava/guava "11.0.1"]]
+  :java-source-path "src/"
+  :repl-init ru.petrsu.nest.yz.momeditor.init-repl)
