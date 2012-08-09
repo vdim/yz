5c026d86fa78ff16d838f2bc3ca6e2afddcf24b1
vdim <vyacheslav.dimitrov@gmail.com>
2012-08-09 10:47:12 +0400

    Added tests for the QueryYZ class.

---
+++ test/src/ru/petrsu/nest/yz/queryyz.clj
@@ -0,0 +1,52 @@
+;;
+;; Copyright 2012 Vyacheslav Dimitrov <vyacheslav.dimitrov@gmail.com>
+;;
+;; This file is part of YZ.
+;;
+;; YZ is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU Lesser General Public License version 3
+;; only, as published by the Free Software Foundation.
+;;
+;; YZ is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with YZ.  If not, see
+;; <http://www.gnu.org/licenses/>.
+;;
+
+(ns ru.petrsu.nest.yz.queryyz
+  ^{:author "Vyacheslav Dimitrov"
+    :doc "Tests the QueryYZ class."}
+  (:use ru.petrsu.nest.yz.core 
+        clojure.test)
+  (:require [ru.petrsu.nest.yz.queries.core :as tc] 
+            [ru.petrsu.nest.yz.queries.bd :as bd])
+  (:import (ru.petrsu.nest.yz QueryYZ)))
+
+;; Define entity manager.
+
+(use-fixtures :once (tc/setup-son bd/son))
+
+;; Define the QueryYZ instance.
+(def qyz (QueryYZ. bd/mem "nest.mom"))
+
+
+;; Define tests
+
+(deftest get-result-list
+         ^{:doc "Tests the getResultList method."}
+         (let [f #(tc/eq-colls (.getResultList qyz %1) %2)]
+           (is (f "building" [[bd/b1] [bd/b2]]))
+           (is (f "floor" [[bd/f1_b1] [bd/f2_b1] [bd/f3_b1] [bd/f1_b2]]))
+           (is (f "building (floor)" [[bd/b1 bd/f1_b1] [bd/b1 bd/f2_b1] [bd/b1 bd/f3_b1] [bd/b2 bd/f1_b2]]))))
+
+
+(deftest get-flat-list
+         ^{:doc "Tests the getFlatList method."}
+         (let [f #(tc/eq-colls (.getFlatResult qyz %1) %2)]
+           (is (f "building" [bd/b1 bd/b2]))
+           (is (f "floor" [bd/f1_b1 bd/f2_b1 bd/f3_b1 bd/f1_b2]))
+           (is (f "building (floor)" [bd/b1 bd/f1_b1 bd/b1 bd/f2_b1 bd/b1 bd/f3_b1 bd/b2 bd/f1_b2]))))
