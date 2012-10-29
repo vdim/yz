94bc0806e95c156a559d1be6708ed2dd395e088d
vdim <vyacheslav.dimitrov@gmail.com>
2012-10-29 14:47:19 +0300

    Fixed tests due to new API (from previous commit).

--- test/src/ru/petrsu/nest/yz/queryyz.clj
+++ test/src/ru/petrsu/nest/yz/queryyz.clj
@@ -19,26 +19,26 @@
 
 (ns ru.petrsu.nest.yz.queryyz
   ^{:author "Vyacheslav Dimitrov"
-    :doc "Tests the QueryYZ class."}
+    :doc "Tests the YZQuery class."}
   (:use ru.petrsu.nest.yz.core 
         clojure.test)
   (:require [ru.petrsu.nest.yz.queries.core :as tc] 
             [ru.petrsu.nest.yz.queries.bd :as bd])
-  (:import (ru.petrsu.nest.yz QueryYZ)))
+  (:import (ru.petrsu.nest.yz YZQuery)))
 
 ;; Define entity manager.
 
 (use-fixtures :once (tc/setup-son bd/son))
 
-;; Define the QueryYZ instance.
-(def qyz (QueryYZ. bd/mem "nest.mom"))
+;; Define the YZQuery instance.
+(def qyz (YZQuery. "nest.mom" bd/mem))
 
 
 ;; Define tests
 
 (deftest get-result-list
          ^{:doc "Tests the getResultList method."}
-         (let [f #(tc/eq-colls (.getResultList qyz %1) %2)]
+         (let [f #(tc/eq-colls (.getResultList (.create qyz %1)) %2)]
            (is (f "building" [[bd/b1] [bd/b2]]))
            (is (f "floor" [[bd/f1_b1] [bd/f2_b1] [bd/f3_b1] [bd/f1_b2]]))
            (is (f "building (floor)" [[bd/b1 bd/f1_b1] [bd/b1 bd/f2_b1] [bd/b1 bd/f3_b1] [bd/b2 bd/f1_b2]]))))
@@ -46,7 +46,7 @@
 
 (deftest get-flat-list
          ^{:doc "Tests the getFlatList method."}
-         (let [f #(tc/eq-colls (.getFlatResult qyz %1) %2)]
+         (let [f #(tc/eq-colls (.getFlatResult (.create qyz %1)) %2)]
            (is (f "building" [bd/b1 bd/b2]))
            (is (f "floor" [bd/f1_b1 bd/f2_b1 bd/f3_b1 bd/f1_b2]))
            (is (f "building (floor)" [bd/b1 bd/f1_b1 bd/b1 bd/f2_b1 bd/b1 bd/f3_b1 bd/b2 bd/f1_b2]))))
